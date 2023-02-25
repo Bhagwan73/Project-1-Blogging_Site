5c51171f00560cdeefe29a8f0ad41c562554dabf
Bhagwan73 <bhagwan735025@gmail.com>
2023-02-25 21:05:31 +0530

    blogging_site

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ const mongoose = require('mongoose')
 const route=require('./route/route')
 
 const app=express()
-
 app.use(bodyParser.json())
 
 mongoose.connect('mongodb+srv://sarwjeet424:96568437528p@cluster0.8tsocgw.mongodb.net/sarwjeet43', {useNewUrlParser: true})
 .then(()=> console.log("MongoDB connected"))
 .catch((error)=> console.log(error))
 
+app.use('/',route)
+
 app.listen(3000,function (){
    console.log("Port connected to 3000")
 })  
 
-app.use('/',route)
+
 
--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require("mongoose")
 
 const authorModel = new mongoose.Schema({
